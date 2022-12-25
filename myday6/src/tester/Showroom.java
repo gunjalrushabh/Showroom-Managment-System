3bbd46012a29cf39690761665cf5cf4e73d04918
gunjalrushabh <rushabhgunjalrg17@gmail.com>
2022-12-26 01:34:50 +0530

    some modification in code

--- myday6/src/tester/Showroom.java
+++ myday6/src/tester/Showroom.java
@@ -1,5 +1,6 @@
 package tester;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -9,6 +10,8 @@ import com.app.Vehicle;
 import cutomException.ShowroomHandlinException;
 import static utills.Validation.validatechasnum;
 import static utills.Validation.validatecolor;
+import static utills.Validation.validateprice;
+import static utills.Validation.validatedate;
 public class Showroom {
 
 	public static void main(String[] args) {
@@ -22,26 +25,37 @@ public class Showroom {
 		while(!exit)
 		{
 			System.out.println("1.add 2.display 10.exit");
+			try {
 			switch(sc.nextInt())
 			{
 			//String chasnum, Color colour, double baseprice, Date mfgdate, String company
 			case 1:
 				if(count<vehicles.length)
 				{
-					
-				Vehicle vobj=new Vehicle(validatechasnum(sc.next(),vehicles), validatecolor(sc.next()), validateprice(sc.nextDouble()), validatedate(sdf.parse(sc.next())), sc.next()) 
+					System.out.println("String chasnum, Color colour, double baseprice, Date mfgdate, String company");
+				Vehicle vobj=new Vehicle(validatechasnum(sc.next(),vehicles), validatecolor(sc.next()), validateprice(sc.nextDouble()), validatedate(sc.next()), sc.next()) ;
 				vehicles[count++]=vobj;
 				}
 				else {
-					throw new ShowroomHandlinException("Showroom is Full")
+					throw new ShowroomHandlinException("Showroom is Full");
 				}
 				break;
 			case 2: 
+				//System.out.println(Arrays.toString(vehicles));
+				for(Vehicle v: vehicles)
+				{
+					System.out.println(v);
+				}
 				break;
 			//case 1: break;
 			case 10:
 				exit =true;
 				break;
+			}
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+				sc.nextLine(); //taking precaution taking i/p o/p mismatching infinite loop switch case
 			}
 		}
 		
